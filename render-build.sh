445cf5b0542c6c0fb7088827a58f68519246f5c7
Anushka142006 <anushkalingayat2006@gmail.com>
2026-02-13 13:36:26 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip to avoid version errors
+pip install --upgrade pip
+
+# Install the corrected requirements.txt
 pip install -r requirements.txt
 
-# This line ensures the YOLO model is ready
+# This line ensures the YOLO model is downloaded and ready on the server
 python -c "from ultralytics import YOLO; YOLO('best_currency_v2.pt')"
